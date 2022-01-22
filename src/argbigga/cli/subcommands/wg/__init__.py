eb8cbd1439ba4709654467e12cdb73c315737b16
David Joaquín Shourabi Porcel <david@djsp.eu>
2022-01-22 20:41:28 +0100

    Add subcommand group `wg`

---
+++ src/argbigga/cli/subcommands/wg/__init__.py
@@ -0,0 +1,10 @@
+import argparse
+import sys
+
+aliases = [
+    'WireGuard',
+]
+description = 'Subcommands for Mullvad deployments based on WireGuard'
+epilog = None
+help = 'WireGuard subcommand group'
+help_formatter_class = argparse.HelpFormatter
